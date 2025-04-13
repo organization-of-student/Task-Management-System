fbf6447cf33b15ce21adc45e97c564cf0fa4c699
Umerubab12 <umerubabjameelmemon@gmail.com>
2025-04-13 23:54:34 +0500

    Delete task.cpp

--- task.cpp
+++
@@ -1,8 +0,0 @@
-#include "task.h"
-
-Task::Task(const QString &title, const QString &description, const QString &dueDate)
-    : title(title), description(description), dueDate(dueDate) {}
-
-QString Task::getTitle() const { return title; }
-QString Task::getDescription() const { return description; }
-QString Task::getDueDate() const { return dueDate; }
